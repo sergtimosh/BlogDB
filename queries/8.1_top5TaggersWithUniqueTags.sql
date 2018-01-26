51d0898fe657162f03a22202b4eb975eef77cc28
Serhiy <sergtimosh@gmail.com>
2018-01-26 09:25:58 +0200

    Update 8.1_top5TaggersWithUniqueTags.sql

--- queries/8.1_top5TaggersWithUniqueTags.sql
+++ queries/8.1_top5TaggersWithUniqueTags.sql
@@ -1,9 +1,9 @@
 SELECT a.auth_id, a.fname, a.lname, COUNT(DISTINCT(pt.tag_id)) tag_number
 FROM author a
 JOIN post p
-ON a.auth_id = p.auth_id
+ ON a.auth_id = p.auth_id
 JOIN post_tag pt
-ON p.post_id = pt.post_id
+ ON p.post_id = pt.post_id
 GROUP BY a.auth_id
 ORDER BY tag_number DESC
-LIMIT 5
+LIMIT 5;
